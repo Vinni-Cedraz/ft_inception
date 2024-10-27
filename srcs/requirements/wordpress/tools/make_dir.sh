d469ca94e69ea2800dcf8ce5f0d0929e0c21ac34
Vinni-Cedraz <planetexpress0101@gmail.com>
2024-10-27 12:13:58 -0300

    simplified worked but images won't load, trying to fix with www-data user

--- srcs/requirements/wordpress/tools/make_dir.sh
+++ srcs/requirements/wordpress/tools/make_dir.sh
@@ -6,5 +6,4 @@ if [ ! -d "/home/vcedraz-/data" ]; then
         mkdir -p /home/vcedraz-/data
         mkdir -p /home/vcedraz-/data/mariadb
         mkdir -p /home/vcedraz-/data/wordpress
-		chown -R $(whoami):$(whoami) /home/vcedraz-/data/
 fi
